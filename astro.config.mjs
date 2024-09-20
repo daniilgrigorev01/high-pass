05b48fd32efce65582cb0ac3b7106928872e2e7a
Daniil Grigorev <daniilgrigorev01@mail.ru>
2024-09-20 20:41:58 +0500

    build: added astro-icon plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    icon({
+      iconDir: 'src/assets/icons',
+    }),
+  ],
   server: {
     host: true,
   },
